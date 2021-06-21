afb056622afc80bdfe8e0eb51d3cb980e07bf9a0
asif-kamal <asif.kamal1489@gmail.com>
2021-06-21 14:10:36 -0400

    Implementing floorArea abstract function within all subclasses

--- luckyNumber/Dwelling.kt
+++ luckyNumber/Dwelling.kt
@@ -1,11 +1,12 @@
 fun main() {
-    val squareCabin = SquareCabin(6)
+    val squareCabin = SquareCabin(6, 50.0)
     
     with(squareCabin){
     println("\nSquare Cabin\n============")
     println("Capacity: ${capacity}")
     println("Material: ${buildingMaterial}")
     println("Has room? ${hasRoom()}")
+    println("Floor area: ${floorArea()}")
     }
    
    val roundHut = RoundHut(3)
@@ -32,14 +33,20 @@ abstract class Dwelling(private var residents: Int) {
     abstract val buildingMaterial: String
     abstract val capacity: Int
     
+    abstract fun floorArea(): Double
+    
     fun hasRoom(): Boolean {
         return residents < capacity
     } 
 }
 
-class SquareCabin(residents: Int) : Dwelling(residents) {
+class SquareCabin(residents: Int, val length: Double) : Dwelling(residents) {
     override val buildingMaterial = "Wood"
     override val capacity = 6
+    
+    override fun floorArea(): Double {
+        return length * length
+    }
 }
 
 open class RoundHut(residents: Int) : Dwelling(residents) {
