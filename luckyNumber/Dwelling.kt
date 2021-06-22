e4c523a2431c2b5c815715039934fb3113ac2ba5
asif-kamal <asif.kamal1489@gmail.com>
2021-06-22 10:26:19 -0400

    Updated RoundHut to take in radius as a double

--- luckyNumber/Dwelling.kt
+++ luckyNumber/Dwelling.kt
@@ -53,7 +53,7 @@ class SquareCabin(residents: Int, val length: Double) : Dwelling(residents) {
     }
 }
 
-open class RoundHut(residents: Int) : Dwelling(residents) {
+open class RoundHut(residents: Int, val radius: Double) : Dwelling(residents) {
     override val buildingMaterial = "Straw"
     override val capacity = 4
     
