80576d59ada5e15d409f51e5226489e310dd37e0
asif-kamal <asif.kamal1489@gmail.com>
2021-06-22 10:19:12 -0400

    Show only couple of decimal places for floor area

--- luckyNumber/Dwelling.kt
+++ luckyNumber/Dwelling.kt
@@ -8,7 +8,7 @@ fun main() {
     println("Capacity: ${capacity}")
     println("Material: ${buildingMaterial}")
     println("Has room? ${hasRoom()}")
-    println("Floor area: %.2f".format(floorArea()))
+    println("Floor area: ${floorArea()})
     }
    
    val roundHut = RoundHut(3, 10.0)
