32cbd31abdba6b629cb9e2ccc69c978e560ed1ef
asif-kamal <asif.kamal1489@gmail.com>
2021-06-22 10:12:20 -0400

    Calculate floor area of each subclass

--- luckyNumber/Dwelling.kt
+++ luckyNumber/Dwelling.kt
@@ -1,3 +1,5 @@
+import kotlin.math.PI
+
 fun main() {
     val squareCabin = SquareCabin(6, 50.0)
     
@@ -6,25 +8,27 @@ fun main() {
     println("Capacity: ${capacity}")
     println("Material: ${buildingMaterial}")
     println("Has room? ${hasRoom()}")
-    println("Floor area: ${floorArea()}")
+    println("Floor area: %.2f".format(floorArea()))
     }
    
-   val roundHut = RoundHut(3)
+   val roundHut = RoundHut(3, 10.0)
    
    with(roundHut){
        println("\nRound Hut\n=========")
        println("Material: ${buildingMaterial}")
        println("Capacity: ${capacity}")
        println("Has room? ${hasRoom()}")
+       println("Floor area: %.2f".format(floorArea()))
    }
    
-   val roundTower = RoundTower(4)
+   val roundTower = RoundTower(4, 15.5)
    
    with(roundTower){
        println("\nRound Tower\n==========")
        println("Material: ${buildingMaterial}")
        println("Capacity: ${capacity}")
        println("Has room? ${hasRoom()}")
+       println("Floor area: %.2f".format(floorArea()))
    }
 }
 
@@ -49,12 +53,21 @@ class SquareCabin(residents: Int, val length: Double) : Dwelling(residents) {
     }
 }
 
-open class RoundHut(residents: Int) : Dwelling(residents) {
+open class RoundHut(residents: Int, val radius: Double) : Dwelling(residents) {
     override val buildingMaterial = "Straw"
     override val capacity = 4
+    
+    override fun floorArea(): Double {
+        return PI * radius * radius
+    }
 }
 
-class RoundTower(residents: Int, val floors: Int = 2) : RoundHut(residents) {
-    override val buildingMaterial = "Stone"
-    override val capacity = 4 * floors
+class RoundTower(residents: Int, radius: Double, 
+                 val floors: Int = 2) : RoundHut(residents, radius) {
+    				override val buildingMaterial = "Stone"
+    				override val capacity = 4 * floors
+    
+    				override fun floorArea(): Double {
+                        return super.floorArea() * floors
+                    }
 }
